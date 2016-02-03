31b7f0578ec8f7bfb563a27485e4ddc1bc8b9089
Nicholas Hemsley <nick.hems@gmail.com>
2016-02-03 15:35:18 +0800

    remove fs111 specific changes, add in rbenv

--- aliases.sh
+++ aliases.sh
@@ -1,14 +1,7 @@
 alias ls="ls --color=auto"
 alias ..="cd .."
 alias ltr="ls -ltr"
-alias ll="ls -l"
-alias g="gvim"
-alias r="gvim --remote-silent"
-alias eoip="ssh -2 -N -l fs111 -D7070 83.169.35.107 -p 443"
-alias play='mplayer -playlist <(find "$PWD" -type f -iname "*.mp3" -o -iname "*.ogg" -o -iname "*.flac" | sort -n)'
-alias jsonlint="python -m simplejson.tool"
-alias yup="yum -y"
-alias crs="kill -10 $(pidof resizR)"
+alias ll="ls -lha"
 
 
 DISTRIBUTION=$(lsb_release -i | cut -d\: -f 2 | tr -d "\t")
