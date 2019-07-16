efe54296120410985f2ebdc960fe94e85f44654a
Nicholas Hemsley <nick.hems@gmail.com>
2019-07-16 22:39:32 +1000

    remove ack alias;

--- aliases.sh
+++ aliases.sh
@@ -3,16 +3,6 @@ alias ..="cd .."
 alias ltr="ls -ltr"
 alias ll="ls -lha"
 
-
-DISTRIBUTION=$(lsb_release -i | cut -d\: -f 2 | tr -d "\t")
-
-if [ $DISTRIBUTION != "Fedora" ]
-then
-    alias ack=ack-grep
-fi
-unset DISTRIBUTION
-
 alias serve_http="ruby -run -ehttpd . -p8000"
 
 alias clipboard='xclip -sel clip'
-
